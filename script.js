6c9bb2622d28a5586e61e8c9113ec5b80499d5ea
alexander <alexandernyaga1999@gmail.com>
2023-10-03 11:18:39 +0300

    method to prevent default

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let form = document.querySelector("form");
 
 // Add an addEventListener to the form submission event
 form.addEventListener("submit", (e) => {
-    //prevent the default form submission behavoiur
+    //prevent the default form submission behavior
+    e.preventDefault()
 })
