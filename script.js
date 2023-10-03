4cd84aa327adbc724ea06cbf3a197fb895f8fd9a
alexander <alexandernyaga1999@gmail.com>
2023-10-03 11:17:12 +0300

    add an event listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //Get the form element(grab the form from Html)
 let form = document.querySelector("form");
 
+// Add an addEventListener to the form submission event
+form.addEventListener("submit", (e) => {
+    //prevent the default form submission behavoiur
+})
