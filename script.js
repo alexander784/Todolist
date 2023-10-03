17a729e7f92de7e6892b38913deb6e0d88edc9b9
alexander <alexandernyaga1999@gmail.com>
2023-10-03 11:28:19 +0300

    send the to-do task to the server

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ form.addEventListener("submit", (e) => {
 //Get the To-do task from the form
 const task = e.target.taskInput.value
 
-//
+//send the to-do task to the server
+// "/todo" is the url endpoint we are sending the reuest to.
+fetch("/todo", {
+    //POST method is used to send data to the server
+
+    method:"POST",
+
+    //Specifies the format of the data we are sending. In this case its json data 
+    headers: {
+        "Content-Type":"application/json",
+    },
+    //The data we are sending.Its converted to JSON string using JSON.stringify"
+    //Send an object with a "task" property, which contains the task data collected from the form.
+    
+})
